c18f2a20deb2a9844a4770e61b5b52e82c9f8b8a
mrakotomizao <mrakotomizao@gmail.com>
2016-03-03 18:31:59 +0100

    commit de la mort qui tue

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,7 +18,8 @@ class AppKernel extends Kernel
             new AppBundle\AppBundle(),
             new UserBundle\UserBundle(),
             new \FOS\UserBundle\FOSUserBundle(),
-            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new \JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new FlickrBundle\FlickrBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
@@ -26,6 +27,8 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new \Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle();
+
         }
 
         return $bundles;
--- web/assets/js/app.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by fylippe on 03/03/2016.
- */
-
-
-$( document ).ready(function() {
-    $('.modal-trigger').leanModal();
-});
