429882827b9a68bcc171e855414fbf0d1212c496
mrakotomizao <mrakotomizao@gmail.com>
2016-03-04 11:09:50 +0100

    Ajout de certaines requetes de Favoris

--- src/AppBundle/Repository/BookmarkRepository.php
+++ src/AppBundle/Repository/BookmarkRepository.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\Bookmark;
 use Doctrine\ORM\EntityRepository;
 use FOS\UserBundle\Model\User;
 
@@ -21,5 +22,33 @@ class BookmarkRepository extends EntityRepository
             ->setParameter('user', $user)
             ->getQuery()
             ->getResult();
+    }
+
+    public function add(User $user, $bookmarkname)
+    {
+        $newBookmark = new Bookmark();
+        $newBookmark->setTitle($bookmarkname);
+        $newBookmark->setUser($user);
+        $newBookmark->setIsActive(true);
+
+        $em = $this->getEntityManager();
+        $em->persist($newBookmark);
+        $em->flush();
+    }
+
+    public function findAllActiveBookmark(User $user)
+    {
+        return $this->createQueryBuilder('b')
+            ->select('b')
+            ->where('b.isActive = :active')
+            ->andWhere('b.User = :user')
+            ->setParameter('active', 1)
+            ->setParameter('user', $user)
+            ->getQuery()
+            ->getResult();
+    }
+
+    public function findAllPictureForUser(){
+
     }
 }
