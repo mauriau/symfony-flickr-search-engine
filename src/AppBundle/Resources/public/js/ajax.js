c8abf10e0cdc0a67f6759cab0e4817c072a82b61
mauriau <maxime.auriau@gmail.com>
2016-03-04 11:27:20 +0100

    udpate display image after search

--- src/AppBundle/Resources/public/js/ajax.js
+++ src/AppBundle/Resources/public/js/ajax.js
@@ -1,14 +1,12 @@
 $(document).ready(function () {
     var sfield = $("#s");
-    var container = $("#FlickrSearch");
+    var container = $(".row");
     var timer;
-
     function FlickrSearch() {
         $(sfield).addClass("loading");
         $(container).empty();
         var q = $(sfield).val();
-        var url = Routing.generate('flickr_search', { tag: q });
-
+        var url = Routing.generate('flickr_search', {tag: q});
         $.ajax({
             type: 'GET',
             url: url,
@@ -16,8 +14,23 @@ $(document).ready(function () {
             success: function (data) {
                 $(sfield).removeClass("loading");
                 $.each(data.photos.photo, function (i, item) {
-                    var ncode = '<img class="activator" height="300" src="https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg"/>';
-                    $(container).append(ncode);
+                    var title = item.title ? jQuery.trim(item.title).substring(0, 17).split(" ").slice(0, -1).join(" ") + "..." : 'Photo sans titre';
+                    var node = '<div class="col l3 m6 s12">';
+                    node += '<div id="cardPicture" class="card"><div id="FlickrSearch" class="card-image waves-effect waves-block waves-light">';
+                    node += '<img class="activator" height="300" src="https://farm' + item.farm + '.staticflickr.com/' + item.server + '/' + item.id + '_' + item.secret + '.jpg"/>';
+                    node += '</div>';
+                    node += '<div class="card-content" style="height: 150px;">';
+                    node += '<span class="card-title activator grey-text text-darken-4" style="word-wrap: break-word;font-size: 20px;">' + title;
+                    node += '</span>';
+                    node += '<a class="modal-trigger" href="#modal1" >';
+                    node += '<i class="small material-icons right" alt="Favoris" style="color:#ffd740"> star_rate </i>';
+                    node += '</a>';
+                    node += '</div>';
+                    node += '<div class="card-reveal">';
+                    node += '<span class="card-title grey-text text-darken-4" ><i class="material-icons right"> close </i></span >';
+                    node += ' information';
+                    node += '</div></div></div>';
+                    $(container).append(node);
                 });
             },
             error: function (xhr, type, exception) {
@@ -44,11 +57,9 @@ $(document).ready(function () {
             e.preventDefault();
         } else {
             clearTimeout(timer);
-
             timer = setTimeout(function () {
                 FlickrSearch();
             }, 900);
         }
     });
-
 });
